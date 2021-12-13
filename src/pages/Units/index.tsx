2bf796fb51ccfa45297eba3c768d905d05b31b5f
José Ricardo <jricardobgoncalves@gmail.com>
2021-12-13 14:12:33 -0300

    :recycle: Refactor function to functional programming and send required props

--- src/pages/Units/index.tsx
+++ src/pages/Units/index.tsx
@@ -1,9 +1,14 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { AxiosInstance } from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useRouteMatch } from 'react-router-dom';
 import SectionLabel from '../../components/App/SectionLabel';
+import NewUnity from '../../components/Units/NewUnity';
 import UnitsActions from '../../components/Units/UnitsActions';
 import UnitsList from '../../components/Units/UnitsList';
+import { IUnity } from '../../interfaces/IUnity';
 import OnEducaAPI from '../../services/api';
 import { State } from '../../store';
 import { Page } from '../components';
@@ -14,20 +19,24 @@ interface IUnitsRouteParams {
 }
 
 const Units = (): JSX.Element => {
-  const [units, setUnits] = useState([]);
+  const [newUnityIsVisible, setNewUnityIsVisible] = useState(false);
+  const [units, setUnits] = useState<IUnity[]>([]);
   const route = useRouteMatch();
   const { id } = route.params as IUnitsRouteParams;
 
   const { subject } = useSelector((store: State) => store);
 
-  const getUnits = async (): Promise<void> => {
-    await OnEducaAPI.get(`/units/subject/${id}`).then((response) => {
-      setUnits(response.data);
+  const getUnits = async (
+    API: AxiosInstance,
+    setUnitsState: (value: IUnity[]) => void,
+  ): Promise<void> => {
+    await API.get(`/units/subject/${id}`).then((response) => {
+      setUnitsState(response.data);
     });
   };
 
   useEffect(() => {
-    getUnits();
+    getUnits(OnEducaAPI, setUnits);
   }, []);
 
   return (
@@ -35,7 +44,14 @@ const Units = (): JSX.Element => {
       <PageBox>
         <SectionLabel backLink="/learn" label={`${subject.name} - unidades`} />
         <UnitsBox>
-          <UnitsActions />
+          <UnitsActions setNewUnityIsVisible={setNewUnityIsVisible} />
+          {newUnityIsVisible && (
+            <NewUnity
+              setNewUnityIsVisible={setNewUnityIsVisible}
+              getUnits={getUnits}
+              setUnits={setUnits}
+            />
+          )}
           <UnitsList units={units} />
         </UnitsBox>
       </PageBox>
