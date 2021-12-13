e6f58e84548c73e07522f4094c35c3c2c3a6d5cd
José Ricardo <jricardobgoncalves@gmail.com>
2021-12-13 14:02:28 -0300

    :sparkles: Common component for text fields

---
+++ src/components/UpdateProfile/UpdateProfileTextInputSection/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  UpdateProfileInputText,
+  UpdateProfileSectionBox,
+  UpdateProfileSectionBoxLabel,
+} from '../styles';
+
+interface IUpdateProfileTextInputSectionProps {
+  label: string;
+  inputType: string;
+  value: string;
+  setValue: (text: string) => void;
+}
+
+const UpdateProfileTextInputSection = (
+  props: IUpdateProfileTextInputSectionProps,
+): JSX.Element => {
+  const { label, inputType, value, setValue } = props;
+
+  return (
+    <UpdateProfileSectionBox>
+      <UpdateProfileSectionBoxLabel>{label}</UpdateProfileSectionBoxLabel>
+      <UpdateProfileInputText
+        type={inputType}
+        value={value}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+          setValue(event.target.value)
+        }
+      />
+    </UpdateProfileSectionBox>
+  );
+};
+
+export default UpdateProfileTextInputSection;
