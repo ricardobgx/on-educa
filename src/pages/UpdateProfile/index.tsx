1f9eab510ff434f378ec613f1c076e36fc046931
José Ricardo <jricardobgoncalves@gmail.com>
2021-12-13 14:13:04 -0300

    :sparkles: UpdateProfile page with connection with API

---
+++ src/pages/UpdateProfile/index.tsx
@@ -0,0 +1,73 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import SectionLabel from '../../components/App/SectionLabel';
+import UpdateProfileActions from '../../components/UpdateProfile/UpdateProfileActions';
+import UpdateProfileSections from '../../components/UpdateProfile/UpdateProfileSections';
+import { isStudent } from '../../functions/user';
+import { State } from '../../store';
+import { DEFAULT_SCHOOL_GRADE } from '../../store/reducers/schoolGrade';
+import { DEFAULT_TEACHING_TYPE } from '../../store/reducers/teachingType';
+import { Page } from '../components';
+import { PageBox, UpdateProfileBox } from './styles';
+
+const UpdateProfile = (): JSX.Element => {
+  /* Global State */
+
+  const { user, aplication } = useSelector((store: State) => store);
+  const { id } = user;
+  const { userType } = aplication;
+
+  /* Local State */
+
+  const [profilePicture, setProfilePicture] = useState('');
+  const [name, setName] = useState('');
+  const [selectedTeachingTypeId, setSelectedTeachingTypeId] = useState('');
+  const [selectedSchoolGradeId, setSelectedSchoolGradeId] = useState('');
+  const [email, setEmail] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+
+  const updateProfileSectionProps = {
+    profilePicture,
+    setProfilePicture,
+    name,
+    setName,
+    selectedTeachingTypeId,
+    setSelectedTeachingTypeId,
+    selectedSchoolGradeId,
+    setSelectedSchoolGradeId,
+    email,
+    setEmail,
+    newPassword,
+    setNewPassword,
+  };
+
+  useEffect(() => {
+    setProfilePicture(user.profilePicture);
+    setName(user.name);
+    if (isStudent(userType)) {
+      const teachingType = user.teachingType || DEFAULT_TEACHING_TYPE;
+      setSelectedTeachingTypeId(teachingType.id);
+    } else {
+      const schoolGrade = user.schoolGrade || DEFAULT_SCHOOL_GRADE;
+      setSelectedSchoolGradeId(schoolGrade.id);
+    }
+    setEmail(user.email);
+  }, [user]);
+
+  return (
+    <Page>
+      <PageBox>
+        <SectionLabel label="Atualizar conta" backLink={`/profile/${id}`} />
+        <UpdateProfileBox>
+          <UpdateProfileSections {...updateProfileSectionProps} />
+          <UpdateProfileActions />
+        </UpdateProfileBox>
+      </PageBox>
+    </Page>
+  );
+};
+
+export default UpdateProfile;
---
+++ src/pages/UpdateProfile/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const PageBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  margin: 20px;
+
+  @media (max-width: 1150px) {
+    flex-wrap: wrap;
+  }
+`;
+export const UpdateProfileBox = styled.div`
+  margin: 20px;
+`;
