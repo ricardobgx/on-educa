9f2957a3b0574dd6ba477f6876b0fce859302fe0
José Ricardo <jricardobgoncalves@gmail.com>
2021-12-13 13:44:47 -0300

    :recycle: Add required fields for student

--- src/components/Duel/DashBoard/index.tsx
+++ src/components/Duel/DashBoard/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { IStudent } from '../../../interfaces/IStudent';
+import { DEFAULT_SCHOOL_GRADE } from '../../../store/reducers/schoolGrade';
+import { DEFAULT_TEACHING_TYPE } from '../../../store/reducers/teachingType';
 import SectionLabel from '../../App/SectionLabel';
 import ParticipantsList from '../ParticipantsList';
 import {
@@ -21,6 +23,7 @@ import {
 
 const teamAParticipants: IStudent[] = [
   {
+    id: 'cdcdcd-ccac-caccacsaa',
     email: 'teste1@gmail.com',
     name: 'Aluno Fulano Sicrano da Silva',
     schoolGradeId: 'ccscd1-csdcsdcsc-cdcssdcsc',
@@ -28,8 +31,11 @@ const teamAParticipants: IStudent[] = [
     isOnline: true,
     profilePicture:
       'https://i.pinimg.com/474x/a2/92/de/a292de2720b31e18ceb366e5ca343fd0.jpg',
+    teachingType: DEFAULT_TEACHING_TYPE,
+    schoolGrade: DEFAULT_SCHOOL_GRADE,
   },
   {
+    id: 'cdcdcd-ccac-caccacsaa',
     email: 'teste2@gmail.com',
     name: 'Aluno Fulano Sicrano da Silva',
     schoolGradeId: 'ccscd1-csdcsdcsc-cdcssdcsc',
@@ -37,8 +43,11 @@ const teamAParticipants: IStudent[] = [
     isOnline: true,
     profilePicture:
       'https://i.pinimg.com/474x/a2/92/de/a292de2720b31e18ceb366e5ca343fd0.jpg',
+    teachingType: DEFAULT_TEACHING_TYPE,
+    schoolGrade: DEFAULT_SCHOOL_GRADE,
   },
   {
+    id: 'cdcdcd-ccac-caccacsaa',
     email: 'teste3@gmail.com',
     name: 'Aluno Fulano Sicrano da Silva',
     schoolGradeId: 'ccscd1-csdcsdcsc-cdcssdcsc',
@@ -46,8 +55,11 @@ const teamAParticipants: IStudent[] = [
     isOnline: true,
     profilePicture:
       'https://i.pinimg.com/474x/a2/92/de/a292de2720b31e18ceb366e5ca343fd0.jpg',
+    teachingType: DEFAULT_TEACHING_TYPE,
+    schoolGrade: DEFAULT_SCHOOL_GRADE,
   },
   {
+    id: 'cdcdcd-ccac-caccacsaa',
     email: 'teste4@gmail.com',
     name: 'Aluno Fulano Sicrano da Silva',
     schoolGradeId: 'ccscd1-csdcsdcsc-cdcssdcsc',
@@ -55,6 +67,8 @@ const teamAParticipants: IStudent[] = [
     isOnline: true,
     profilePicture:
       'https://i.pinimg.com/474x/a2/92/de/a292de2720b31e18ceb366e5ca343fd0.jpg',
+    teachingType: DEFAULT_TEACHING_TYPE,
+    schoolGrade: DEFAULT_SCHOOL_GRADE,
   },
 ];
 
