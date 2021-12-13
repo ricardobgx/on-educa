0826d69de3effea7d81dab3844158e40509ee599
José Ricardo <jricardobgoncalves@gmail.com>
2021-12-13 13:43:32 -0300

    :recycle: Disable eslint rules

--- src/components/Content/ContentAccordion/index.tsx
+++ src/components/Content/ContentAccordion/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import React, { useEffect, useState } from 'react';
 import { IContent } from '../../../interfaces/IContent';
 import { IUnity } from '../../../interfaces/IUnity';
