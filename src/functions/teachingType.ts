a3383678386521b7fe51dbc1b1835bcf57d3b40f
José Ricardo <jricardobgoncalves@gmail.com>
2021-12-13 14:03:41 -0300

    :sparkles: Functions for get teaching types from API

---
+++ src/functions/teachingType.ts
@@ -0,0 +1,25 @@
+import { AxiosInstance } from 'axios';
+import { ITeachingType } from '../interfaces/ITeachingType';
+
+// GET
+
+export const getTeachingTypes = async (
+  API: AxiosInstance,
+  setTeachingTypesState: (teachingTypes: ITeachingType[]) => void,
+): Promise<void> => {
+  await API.get('/teachingtypes').then((response) => {
+    setTeachingTypesState(response.data);
+  });
+};
+
+// SHOW
+
+export const getTeachingType = async (
+  API: AxiosInstance,
+  id: string,
+  setTeachingTypeState: (teachingType: ITeachingType) => void,
+): Promise<void> => {
+  await API.get(`/teachingtypes/${id}`).then((response) => {
+    setTeachingTypeState(response.data);
+  });
+};
