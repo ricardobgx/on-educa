bdb7d77b3f451689f283611dca23592e6cbf3ebe
José Ricardo <jricardobgoncalves@gmail.com>
2021-12-13 13:43:00 -0300

    :recycle: Remove unused variable

--- src/components/App/UserCard/styles.ts
+++ src/components/App/UserCard/styles.ts
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 import { mediumIcon, paragraph } from '../../../components';
 
 export const UserCardBox = styled.div`
