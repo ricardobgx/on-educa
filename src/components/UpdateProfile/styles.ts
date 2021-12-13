c9d2b54cc8f4511f6137f049ea3461b51051149a
José Ricardo <jricardobgoncalves@gmail.com>
2021-12-13 13:59:44 -0300

    :sparkles: HTML elements common for UpdateProfile components

---
+++ src/components/UpdateProfile/styles.ts
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import {
+  inputSelect,
+  inputText,
+  paragraph,
+  SectionBox,
+} from '../../components';
+
+export const UpdateProfileSectionLabel = styled(paragraph)`
+  color: ${({ theme }) => theme.colors.textColor};
+  font-weight: bold;
+`;
+export const UpdateProfileSection = styled(SectionBox)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: calc(50% - 20px);
+  padding: 20px;
+  margin-bottom: 20px;
+  background: ${({ theme }) => theme.colors.boxColor};
+  min-height: calc(100vh - 285px);
+
+  @media (max-width: 1150px) {
+    width: 100%;
+    min-height: unset;
+  }
+`;
+export const UpdateProfileSectionsBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+export const UpdateProfileSectionBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+`;
+export const UpdateProfileSectionBoxLabel = styled(paragraph)`
+  color: ${({ theme }) => theme.colors.textColor};
+  font-weight: bold;
+`;
+export const UpdateProfileInputText = styled(inputText)`
+  color: ${({ theme }) => theme.colors.textColor};
+  background: ${({ theme }) => theme.colors.boxColor};
+  /* box-shadow: 0px 0px 4px ${({ theme }) => theme.colors.boxShadowColor}; */
+  border: 2px solid ${({ theme }) => theme.colors.textColor};
+  height: 40px;
+  border-radius: 5px;
+  padding: 0 10px;
+  margin-top: 10px;
+`;
+export const UpdateProfileSelect = styled(inputSelect)`
+  color: ${({ theme }) => theme.colors.boxColor};
+  background: ${({ theme }) => theme.colors.textColor};
+  /* box-shadow: 0px 0px 4px ${({ theme }) => theme.colors.boxShadowColor}; */
+  border-right: 10px solid transparent;
+  height: 40px;
+  border-radius: 5px;
+  padding: 0 10px;
+  margin-top: 10px;
+`;
+export const UpdateProfileOption = styled.option``;
