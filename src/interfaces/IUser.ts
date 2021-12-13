a7ae9309766ad99f9f0c76ad94804f026d56db5d
José Ricardo <jricardobgoncalves@gmail.com>
2021-12-13 14:07:18 -0300

    :recycle: Remove token field and alter teachingTypeId and schoolGradeId to required fields

--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -1,10 +1,13 @@
+import { ISchoolGrade } from './ISchoolGrade';
+import { ITeachingType } from './ITeachingType';
+
 export interface IUser {
+  id: string;
   email: string;
   name: string;
   profilePicture: string;
   isOnline: boolean;
   league: string;
-  token?: string;
-  teachingTypeId?: string;
-  schoolGradeId?: string;
+  teachingType: ITeachingType;
+  schoolGrade: ISchoolGrade;
 }
