dc65f9170e6464fafef3b8a0fc5b481ccd04fae7
José Ricardo <jricardobgoncalves@gmail.com>
2022-01-13 18:58:21 -0300

    :sparkles: Create function to verify if the user is the owner of duel

--- src/functions/duel.ts
+++ src/functions/duel.ts
@@ -2,6 +2,24 @@ import { AxiosError, AxiosInstance } from 'axios';
 import { IDuelParams } from '../dto/IDuelParams';
 import { IDuel } from '../interfaces/IDuel';
 
+// Funcoes da aplicacao
+
+/**
+ * @author Jose Ricardo Brasileiro Goncalves
+ * @modified 12/01/2022
+ * @param studentId Recebe o id de um estudante
+ * @param duelOwnerId Recebe o id do estudante dono do duelo
+ * @brief Verifica se o estudante eh o dono do duelo
+ * @returns Retorna true caso o estudante seja o dono e false caso contrario
+ */
+
+export const isDuelOwner = (
+  studentId: string,
+  duelOwnerId: string,
+): boolean => {
+  return studentId === duelOwnerId;
+};
+
 /* API functions */
 
 // Create duel
