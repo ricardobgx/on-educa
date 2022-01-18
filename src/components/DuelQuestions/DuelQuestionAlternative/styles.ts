2e573e4e943552758be0a6bca5e63ef4fb31be78
José Ricardo <jricardobgoncalves@gmail.com>
2022-01-18 15:58:21 -0300

    :recycle: Update the alternative component style

--- src/components/DuelQuestions/DuelQuestionAlternative/styles.ts
+++ src/components/DuelQuestions/DuelQuestionAlternative/styles.ts
@@ -2,11 +2,11 @@ import styled from 'styled-components';
 import { paragraph } from '../../../global/styles/components/textComponents';
 
 export const Container = styled.button`
-  border: none;
+  border: 3px solid transparent;
   background: ${({ theme }) => theme.colors.boxColor};
   color: ${({ theme }) => theme.colors.questionTextColor};
   box-shadow: 0px 0px 4px ${({ theme }) => theme.colors.boxShadowColor};
-  padding: 10px 20px;
+  padding: 10px 15px;
   margin-bottom: 20px;
   border-radius: 5px;
   cursor: pointer;
